0ce25624338b61a229cf0f34ee25a456501e55bb
Dima Y <dmytroyeroshenko@xiaomirepeater_v3.home>
2021-10-31 22:25:13 +0100

    Testing django.signals to async handle POST delay

--- codingskills/checker/execute_test.py
+++ codingskills/checker/execute_test.py
@@ -1,5 +1,6 @@
-from .handlers import ContainerHandler, FilesHandler
 import uuid
+
+from .handlers import ContainerHandler, FilesHandler
 
 class ExecuteTest:
     def __init__(self, input, output, code, language):
@@ -11,14 +12,20 @@ class ExecuteTest:
         self.__container_handler = ContainerHandler(self.__language, self.__container_name)
         self.__files_handler = FilesHandler(self.__input, self.__output, self.__code, self.__language, self.__container_name)
 
+    @property
+    def container_handler(self):
+        return self.__container_handler
+
+    @property
+    def files_handler(self):
+        return self.__files_handler
+
     def run_test(self):
         self.__container_handler.run_container()
         self.__files_handler.generate_all_files()
         self.__files_handler.copy_files_to_container()
         self.__container_handler.run_compiler()
         self.__files_handler.copy_result_from_container()
-        test_result = self.__files_handler.get_result_value()
-        self.__container_handler.stop_container()
-        self.__files_handler.cleanup_temp_files()
+        
+        return self.__files_handler.get_result_value()
 
-        return test_result
--- codingskills/checker/views.py
+++ codingskills/checker/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.views.generic import View
+from django.core.signals import request_finished
+
 import json
 
 from .execute_test import ExecuteTest
+
 
 class HandleFrontendData(View):
     def post(self, request, *args, **kwargs):
@@ -14,6 +17,10 @@ class HandleFrontendData(View):
         task_code = str(data['task_code'])
         task_language = str(data['task_language']).lower()
 
-        test_result = ExecuteTest(task_input, task_output, task_code, task_language).run_test()
-        
-        return HttpResponse(test_result)
+        executeTest = ExecuteTest(task_input, task_output, task_code, task_language)
+        result = executeTest.run_test()
+
+        # request_finished.connect(executeTest.container_handler.stop_container(), executeTest.files_handler.cleanup_temp_files())
+        return HttpResponse(result)
+
+        #Check with multithreading vs django.signals
