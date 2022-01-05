bf53a57398299c5d316843b36ad5c73a25f3f207
Dima Y <dmytroyeroshenko@Dimas-MacBook-Pro.local>
2022-01-05 22:39:20 +0000

    Added models and views testing

--- codingskills/exercises/tests.py
+++ codingskills/exercises/tests.py
@@ -1,12 +1,62 @@
 from django.test import TestCase, Client
 
+'''
+Test views
+'''
+
 class TestHomeView(TestCase):
     HOME_URL = '/'
 
     def setUp(self):
         self.client = Client()
 
-    def test_should_return_200_when_app_is_running(self):
+    def test_should_return_200_when_app_is_running(self) -> None:
         response = self.client.get(TestHomeView.HOME_URL)
 
         self.assertEqual(response.status_code, 200)
+
+class TestExercisesDetailView(TestCase):
+    HOME_URL = '/palindrome'
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_should_return_200_when_palindrome_window_is_rendered(self) -> None:
+        response = self.client.get(TestHomeView.HOME_URL)
+
+        self.assertEqual(response.status_code, 200)
+
+'''
+Test models
+'''
+from models import Tasks, Tests
+
+class TestTasksModel(TestCase):
+    @classmethod
+    def generateTestData(cls) -> None:
+        
+        Tasks.objects.create(
+            task_id = 999,
+            task_name = 'Test task',
+            task_content = 'Test task content'
+        )
+
+    def setUp(self) -> None:
+        self.test_task = Tasks.objects.get(task_id = 999)
+
+    def test_get_task_id(self) -> None:
+        self.assertEqual(self.test_task.task_id, 999, msg="Task should equal: 999")
+
+    def test_task_title_content(self) -> None:
+        expected_task_name = "Test Task"
+
+        self.assertEqual(
+            self.test_task.task_name, expected_task_name, msg="Title should be 'Test task'"
+        )
+
+    def test_task_content(self) -> None:
+        expected_task_content = 'Test task content'
+
+        self.assertEqual(
+            self.test_task.task_name, expected_task_content, msg="Content should be 'Test task content'"
+        )
